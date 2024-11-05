78010a9211d1404fa7b5eb9f1d76f450326002eb
Arela-Cristina <instagrandine@gmail.com>
2024-11-05 16:10:28 +0100

    ciclo for: get 10 mails then to call Axios

--- main.js
+++ main.js
@@ -5,3 +5,20 @@
 // Inserire un bottone che al click faccia il fetch di altre 10 mail (sostituendo le altre)
 // Buon Lavoro
 
+
+let mail
+
+for (let i = 0; i < 10; i++) { // 10 itterazioni
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail') //chiamata HTTP => API
+
+        .catch(error => { //controlliamo errori
+            console.log('error')
+        })
+
+        .then(res => {
+            console.log(res.data.response) //debug
+            mail = res.data.response //stampiamo le  mails
+
+        })
+
+}
